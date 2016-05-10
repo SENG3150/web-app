9d428df4246ae175ce0434c534ce0fd46b4e5f2b
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-05-10 20:36:21 +1000

    Added due credit to github issue and solution.

--- js/services/APIService.js
+++ js/services/APIService.js
@@ -5,6 +5,7 @@ angular
 			service: function (object) {
 				var service = Restangular.service(object);
 
+				// Some guidance taken from https://github.com/mgonto/restangular/pull/1134#issuecomment-133371800
 				service.one = function (id) {
 					return Restangular.one(object, id);
 				};

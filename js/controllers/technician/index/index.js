d90f50bcb777d8345870fec81fcbf54e860fbec8
Twunt <bpratten_09@hotmail.com>
2016-05-11 23:53:25 +1000

    Added functionality to select machine and view its major/sub assemblies

--- js/controllers/technician/index/index.js
+++ js/controllers/technician/index/index.js
@@ -1,5 +1,9 @@
 angular
 	.module('joy-global')
-	.controller('TechnicianIndexControllerIndex', ['$scope', 'AuthService', function ($scope, AuthService) {
+	.controller('TechnicianIndexControllerIndex', ['$scope', 'Inspections', 'Machines', 'moment', 'AuthService', function ($scope, Inspections, Machine, moment, AuthService) {
 		$scope.user = AuthService.getUser();
+
+		$scope.inspections = Inspections.getList({include: 'machine'}).$object;
+		$scope.machines = Machine.getList({include: 'model.majorAssemblies.subAssemblies'}).$object;
+		
 	}]);

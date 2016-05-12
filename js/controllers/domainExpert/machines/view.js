563a21d456563eb49cba1ebb51712f079e7e67a0
Twunt <bpratten_09@hotmail.com>
2016-05-12 17:00:30 +1000

    Fixed machine view so it now displays the machine properly. Gave functionality to navigation tabs

--- js/controllers/domainExpert/machines/view.js
+++ js/controllers/domainExpert/machines/view.js
@@ -1,6 +1,8 @@
 angular
 	.module('joy-global')
-	.controller('DomainExpertMachinesControllerView', ['$scope', 'Inspections', 'Machines', function ($scope, Inspections, Machines) {
+	.controller('DomainExpertMachinesControllerView', ['$scope', 'Inspections', 'Machines', '$stateParams', function ($scope, Inspections, Machines, $stateParams) {
 		$scope.inspections = Inspections.getList({include: 'machine'}).$object;
-		$scope.machines = Machines.getList({include: 'model.majorAssemblies.subAssemblies'}).$object;
+		//$scope.machines = Machines.getList({include: 'model.majorAssemblies.subAssemblies'}).$object;
+		$scope.machineId = $stateParams.id;
+		$scope.machine = Machines.one($scope.machineId).get({include: 'model.majorAssemblies.subAssemblies'}).$object;
 	}]);

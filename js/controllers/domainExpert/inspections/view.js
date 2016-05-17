f999c1386fc87b8a7ea1477391081f26da144362
Phasedd <Phasedd@users.noreply.github.com>
2016-05-17 20:20:02 +1000

    Added toggable panels to Domain Expert Inspections view

--- js/controllers/domainExpert/inspections/view.js
+++ js/controllers/domainExpert/inspections/view.js
@@ -4,7 +4,7 @@ angular
 		$scope.inspectionId = $stateParams.id;
 
 		$scope.inspection = Inspections.one($scope.inspectionId).get({
-			include: 'majorAssemblies.majorAssembly,majorAssemblies.subAssemblies.subAssembly'
+			include: 'majorAssemblies.majorAssembly,majorAssemblies.subAssemblies.subAssembly.tests'
 		}).$object;
 		
 		$scope.moment = moment;

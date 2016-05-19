1a0092ad54c58ce1bf19ad9b7d3eddb3a134b72d
Phasedd <Phasedd@users.noreply.github.com>
2016-05-19 22:00:49 +1000

    Added the user details to the comment data to be uploaded

--- js/controllers/domainExpert/inspections/view.js
+++ js/controllers/domainExpert/inspections/view.js
@@ -1,6 +1,6 @@
 angular
 	.module('joy-global')
-	.controller('DomainExpertInspectionsControllerView', ['$scope', 'Inspections', 'moment', '$stateParams', 'LayoutService', 'toastr', 'Comments', function ($scope, Inspections, moment, $stateParams, LayoutService, toastr, Comments) {
+	.controller('DomainExpertInspectionsControllerView', ['$scope', 'Inspections', 'moment', '$stateParams', 'LayoutService', 'toastr', 'Comments', 'AuthService', function ($scope, Inspections, moment, $stateParams, LayoutService, toastr, Comments, AuthService) {
 		$scope.inspectionId = $stateParams.id;
 
 		LayoutService.reset();
@@ -26,9 +26,21 @@ angular
 		}).$object;
 		
 		$scope.moment = moment;
+		$scope.commentData = {
+			author: {}
+		};
 
-		$scope.commentData = {};
 		$scope.submitComment = function() {
+			var user = AuthService.getUser();
+
+			//add the user data to the comment
+			$scope.commentData.type = user.type;
+			$scope.commentData.author.email = user.primary.email;
+			$scope.commentData.author.name = user.primary.name;
+			$scope.commentData.author.firstName = user.primary.firstName;
+			$scope.commentData.author.lastName = user.primary.lastName;
+			$scope.commentData.author.temporary = user.temporary || false;
+
 			console.log($scope.commentData);
 			// save the comment. pass in comment data from the form
 			// use the function we created in our service

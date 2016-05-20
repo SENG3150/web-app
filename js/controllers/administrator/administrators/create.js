6fb9d17edbdfdfe0987ebd56b8559e789808c844
Nick <nickbte@gmail.com>
2016-05-20 18:40:03 +1000

    Changed Admin/create stuff to match the other /create's

--- js/controllers/administrator/administrators/create.js
+++ js/controllers/administrator/administrators/create.js
@@ -1,15 +1,39 @@
 angular
 	.module('joy-global')
-	.controller('AdministratorAdministratorsControllerCreate', ['$scope', 'Administrators', function ($scope, Administrators) {
-		$scope.administrators = Administrators.getList().$object;
-		
+	.controller('AdministratorAdministratorsControllerCreate', ['$scope', 'toastr', function ($scope, toastr) {
 		$scope.newUserData = {};
-		
-		$scope.submitUser = function(){
-			$scope.newUserData.name = $scope.newUserData.firstName + " " + $scope.newUserData.lastName;
-			
-			console.log($scope.newUserData);
+
+		$scope.submitUser = function() {
+			if($scope.validate() == true) {
+				console.log($scope.newUserData);
+			};
 		};
-		
-		
+
+		$scope.validate = function() {
+			if($scope.newUserData.firstname == "" || $scope.newUserData.firstname == null) {
+				toastr.clear();
+				toastr.error("Enter First name");
+
+				return false;
+			}
+			if($scope.newUserData.lastname == "" || $scope.newUserData.lastname == null) {
+				toastr.clear();
+				toastr.error("Enter Last name");
+
+				return false;
+			}
+			if($scope.newUserData.email == "" || $scope.newUserData.email == null) {
+				toastr.clear();
+				toastr.error("Enter a valid email address");
+
+				return false;
+			}
+			if($scope.newUserData.password == "" || $scope.newUserData.password == null) {
+				toastr.clear();
+				toastr.error("Enter a password");
+
+				return false;
+			}
+			return true;
+		}
 	}]);

88b255ed42c015d6f8593b2d0c04598047349e51
Phasedd <Phasedd@users.noreply.github.com>
2016-05-21 18:14:48 +1000

    Added Oil Test Graphs to the Inspections Report Page.

--- js/controllers/domainExpert/inspections/report.js
+++ js/controllers/domainExpert/inspections/report.js
@@ -2,44 +2,14 @@ angular
 	.module('joy-global')
 	.controller('DomainExpertInspectionsControllerReport', ['$scope', 'Inspections', 'moment', '$stateParams', function ($scope, Inspections, moment, $stateParams) {
 		$scope.inspectionId = $stateParams.id;
-
-		$scope.inspection = Inspections.one($scope.inspectionId).get({
+		var baseInspections = Inspections.one($scope.inspectionId).get({
 			include: 'majorAssemblies.majorAssembly,majorAssemblies.subAssemblies.subAssembly'
-		}).$object;
+		});
+		$scope.inspection = baseInspections.$object;
 
 		$scope.moment = moment;
 
-		var datasets = [
-			{
-				data: [
-					1,
-					2,
-					3,
-					4
-				],
-				name: 'Cost',
-				unit: 'Dollars',
-				type: 'column',
-				tooltip: {
-					valuePrefix: '$'
-				}
-			},
-			{
-				data: [
-					5,
-					6,
-					7,
-					8
-				],
-				name: 'Hours',
-				unit: 'Hours',
-				type: 'column',
-				yAxis: 1,
-				tooltip: {
-					valueSuffix: ' hours'
-				}
-			}
-		];
+		$scope.oilTestGraphs = [];
 
 		Highcharts.setOptions({
 			lang: {
@@ -51,62 +21,134 @@ angular
 			}
 		});
 
-		$scope.chartConfig = {
-			options: {
+		createTimeLineGraph = function(graphTitle, traces) {
+			var graphConfig = {
 				chart: {
-					zoomType: 'xy'
+					type: 'spline'
 				},
 				title: {
-					text: 'Spend Report'
+					text: graphTitle
 				},
-				xAxis: [{
-					categories: [
-						'Category 1',
-						'Category 2',
-						'Category 3',
-						'Category 4'
-					],
-					crosshair: true
-				}],
-				yAxis: [
-					{
-						title: {
-							text: 'Cost'
-						},
-						labels: {
-							format: '${value}',
-							style: {
-								color: Highcharts.getOptions().colors[0]
-							}
-						}
-					},
-					{
-						gridLineWidth: 0,
-						title: {
-							text: 'Hours'
-						},
-						labels: {
-							format: '{value} hours',
-							style: {
-								color: Highcharts.getOptions().colors[1]
-							}
-						},
-						opposite: true
+				xAxis: {
+					type: 'datetime',
+					dateTimeLabelFormats: { // don't display the dummy year
+						month: '%e. %b',
+						year: '%b'
 					}
-				],
+				},
+				yAxis: {
+					min: 0
+				},
 				tooltip: {
 					shared: true
 				},
-				legend: {
-					layout: 'vertical',
-					align: 'left',
-					x: 80,
-					verticalAlign: 'top',
-					y: 55,
-					floating: true,
-					backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+				plotOptions: {
+					spline: {
+						marker: {
+							enabled: true
+						}
+					}
+				},
+				series: traces
+			};
+
+			return graphConfig;
+		}
+
+		baseInspections.then(function(inspection) {
+			for (var majorAssemblyID in inspection.majorAssemblies) {
+				var majorAssembly = inspection.majorAssemblies[majorAssemblyID];
+
+				for (var subAssemblyID in majorAssembly.subAssemblies) {
+					var subAssembly = majorAssembly.subAssemblies[subAssemblyID];
+
+					//check to see if the assembly has an oil test to be graphed
+					if (subAssembly.oilTest != null) {
+						var oilTest = subAssembly.oilTest;
+
+						var graphOne = [
+							{
+								name: "Sodium (Na)",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.sodium]
+								]
+							},
+							{
+								name: "Silicon (Si)",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.silicon]
+								]
+							},
+							{
+								name: "aluminium (Al)",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.aluminium]
+								]
+							}
+						];
+
+						var graphTwo = [
+							{
+								name: "Iron (Fe)",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.iron]
+								]
+							},
+							{
+								name: "PQ90",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.pq90]
+								]
+							}
+						];
+
+						var graphThree = [
+							{
+								name: "Lead (Pb)",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.lead]
+								]
+							},
+							{
+								name: "Copper (Cu)",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.copper]
+								]
+							},
+							{
+								name: "Tin (Sn)",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.tin]
+								]
+							}
+						];
+
+						var graphFour = [
+							{
+								name: "Water",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.water]
+								]
+							}
+						];
+
+						var graphFive = [
+							{
+								name: "Viscosity (460)",
+								data: [
+									[$scope.inspection.timeCompleted, oilTest.viscosity]
+								]
+							}
+						];
+
+						//add the graph configs to oilTestGraphs
+						$scope.oilTestGraphs.push(createTimeLineGraph(subAssembly.subAssembly.name, graphOne));
+						$scope.oilTestGraphs.push(createTimeLineGraph(subAssembly.subAssembly.name, graphTwo));
+						$scope.oilTestGraphs.push(createTimeLineGraph(subAssembly.subAssembly.name, graphThree));
+						$scope.oilTestGraphs.push(createTimeLineGraph(subAssembly.subAssembly.name, graphFour));
+						$scope.oilTestGraphs.push(createTimeLineGraph(subAssembly.subAssembly.name, graphFive));
+					}
 				}
-			},
-			series: datasets
-		};
+			}
+		});
 	}]);

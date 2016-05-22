e1d26c6e7ff27f1e1bd23e14a64c7fbb41b31169
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-05-23 02:07:26 +1000

    Minor changes to formatting to match code styling.

--- js/controllers/domainExpert/machines/view.js
+++ js/controllers/domainExpert/machines/view.js
@@ -1,13 +1,12 @@
 angular
 	.module('joy-global')
 	.controller('DomainExpertMachinesControllerView', ['$scope', 'Inspections', 'Machines', '$stateParams', 'LayoutService', function ($scope, Inspections, Machines, $stateParams, LayoutService) {
-		LayoutService.reset();
-
 		$scope.inspections = Inspections.getList({include: 'machine'}).$object;
 		//$scope.machines = Machines.getList({include: 'model.majorAssemblies.subAssemblies'}).$object;
 		$scope.machineId = $stateParams.id;
 		$scope.machine = Machines.one($scope.machineId).get({include: 'model.majorAssemblies.subAssemblies'}).$object;
-		
+
+		LayoutService.reset();
 		LayoutService.setTitle(['Machine ' + $scope.machineId,'Machines']);
 		LayoutService.getPageHeader().setBreadcrumbs([
 			{

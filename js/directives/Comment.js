49149b228a9884c51e136cc889593dad15e4d4ed
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-05-23 02:03:34 +1000

    Minor changes to formatting to match code styling.

--- js/directives/Comment.js
+++ js/directives/Comment.js
@@ -1,35 +1,36 @@
 angular
-    .module('joy-global')
-    .directive('addComment', [function () {
-        return {
-            restrict: "EA",
-            scope: {
-                location: "@",
-                locationId: "@",
-            },
-            controller: ['$scope', 'Comments', 'AuthService', 'moment', 'toastr', function($scope, Comments, AuthService, moment, toastr) {
-                $scope.commentData = {};
+	.module('joy-global')
+	.directive('addComment', [function () {
+		return {
+			restrict: 'EA',
+			scope: {
+				location: '@',
+				locationId: '@'
+			},
+			controller: ['$scope', 'Comments', 'AuthService', 'moment', 'toastr', function ($scope, Comments, AuthService, moment, toastr) {
+				$scope.commentData = {};
 
-                $scope.submitComment = function() {
-                    toastr.info('Attempting to save your comment');
-                    var user = AuthService.getUser();
+				$scope.submitComment = function () {
+					toastr.info('Attempting to save your comment');
 
-                    $scope.commentData[user.type] = user.primary.id;
-                    $scope.commentData.timeCommented = moment().format();
+					var user = AuthService.getUser();
 
-                    if($scope.location != null && $scope.locationId != null) {
-                        $scope.commentData[$scope.location] = $scope.locationId;
-                    }
+					$scope.commentData[user.type] = user.primary.id;
+					$scope.commentData.timeCommented = moment().format();
 
-                    Comments.save($scope.commentData).then(function() {
-                        toastr.clear();
-                        toastr.success('Your comment was saved successfully!');
-                    }, function() {
-                        toastr.clear();
-                        toastr.error('Their was an error saving your comment.');
-                    });;
-                };
-            }],
-            templateUrl: 'views/common/comment.html',
-        };
-    }]);
+					if ($scope.location != null && $scope.locationId != null) {
+						$scope.commentData[$scope.location] = $scope.locationId;
+					}
+
+					Comments.save($scope.commentData).then(function () {
+						toastr.clear();
+						toastr.success('Your comment was saved successfully!');
+					}, function () {
+						toastr.clear();
+						toastr.error('Their was an error saving your comment.');
+					});
+				};
+			}],
+			templateUrl: 'views/common/comment.html'
+		};
+	}]);

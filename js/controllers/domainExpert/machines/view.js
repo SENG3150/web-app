08d0cd291309676f9d047bd289f1e048df16f4fe
Twunt <bpratten_09@hotmail.com>
2016-05-23 19:16:28 +1000

    Added unique details, smu to inspection view page

--- js/controllers/domainExpert/machines/view.js
+++ js/controllers/domainExpert/machines/view.js
@@ -1,26 +1,31 @@
 angular
-	.module('joy-global')
-	.controller('DomainExpertMachinesControllerView', ['$scope', 'Inspections', 'Machines', '$stateParams', 'LayoutService', function ($scope, Inspections, Machines, $stateParams, LayoutService) {
-		$scope.inspections = Inspections.getList({include: 'machine'}).$object;
-		//$scope.machines = Machines.getList({include: 'model.majorAssemblies.subAssemblies'}).$object;
-		$scope.machineId = $stateParams.id;
-		$scope.machine = Machines.one($scope.machineId).get({include: 'model.majorAssemblies.subAssemblies'}).$object;
+    .module('joy-global')
+    .controller('DomainExpertMachinesControllerView', ['$scope', 'Inspections', 'Machines', '$stateParams', 'LayoutService', function ($scope, Inspections, Machines, $stateParams, LayoutService) {
+        $scope.inspections = Inspections.getList({include: 'machine'}).$object;
+        //$scope.machines = Machines.getList({include: 'model.majorAssemblies.subAssemblies'}).$object;
+        $scope.machineId = $stateParams.id;
+        $scope.machine = Machines.one($scope.machineId).get({include: 'model.majorAssemblies.subAssemblies'}).$object;
 
-		LayoutService.reset();
-		LayoutService.setTitle(['Machine ' + $scope.machineId, 'Machines']);
-		LayoutService.getPageHeader().setActionButton('<button type="button" class="btn btn-primary btn-block"><i class="fa fa-plus"></i>Schedule Inspection</button>');
-		LayoutService.getPageHeader().setBreadcrumbs([
-			{
-				route: 'domainExpert-index',
-				displayName: 'Home'
-			},
-			{
-				route: 'domainExpert-machines-index',
-				displayName: 'Machines'
-			},
-			{
-				route: 'domainExpert-machines-view({ id: ' + $scope.machineId + ' })',
-				displayName: 'Machine ' + $scope.machineId
-			}
-		]);
-	}]);
+        LayoutService.reset();
+        LayoutService.setTitle(['Machine ' + $scope.machineId, 'Machines']);
+        LayoutService.getPageHeader().setActionButton('<button type="button" class="btn btn-primary btn-block"><i class="fa fa-calendar-plus-o""></i> Schedule Inspection</button>');
+        $scope.goTo = function () {
+            $state.go('domainExpert-inspections-create({id:' + 1 + ' })');
+        };
+
+        LayoutService.getPageHeader().onClicked($scope.goTo);
+        LayoutService.getPageHeader().setBreadcrumbs([
+            {
+                route: 'domainExpert-index',
+                displayName: 'Home'
+            },
+            {
+                route: 'domainExpert-machines-index',
+                displayName: 'Machines'
+            },
+            {
+                route: 'domainExpert-machines-view({ id: ' + $scope.machineId + ' })',
+                displayName: 'Machine ' + $scope.machineId
+            }
+        ]);
+    }]);

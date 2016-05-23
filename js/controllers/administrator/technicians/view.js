6679fa573cd1b8eccce03b143234a4f7a615a022
Mitchell Davis <mitchell.peter.davis@gmail.com>
2016-05-23 15:13:36 +1000

    Updated styling on technician page. Corrected layout details.

--- js/controllers/administrator/technicians/view.js
+++ js/controllers/administrator/technicians/view.js
@@ -1,10 +1,52 @@
-
 angular
 	.module('joy-global')
-	.controller('AdministratorTechniciansControllerView', ['$scope', 'Technicians' ,'$stateParams', 'LayoutService', 'moment', function ($scope, Technicians ,$stateParams, LayoutService , moment ) {
+	.controller('AdministratorTechniciansControllerView', ['$scope', 'Technicians', '$stateParams', 'LayoutService', 'moment', function ($scope, Technicians, $stateParams, LayoutService, moment) {
 		$scope.technicianId = $stateParams.id;
+		$scope.loading = true;
 
-		$scope.technician = Technicians.one($scope.technicianId).get().$object;
-		$scope.start = moment().subtract(7, 'days'); // This is set to whatever
+		Technicians.one($scope.technicianId).get().then(
+			function(data) {
+				$scope.loading = false;
+				$scope.technician = data;
 
+				LayoutService.setTitle([$scope.technician.name, 'Technicians']);
+				LayoutService.getPageHeader().setBreadcrumbs([
+					{
+						route: 'administrator-index',
+						displayName: 'Home'
+					},
+					{
+						route: 'administrator-technicians-index',
+						displayName: 'Technicians'
+					},
+					{
+						route: 'administrator-technicians-view',
+						displayName: $scope.technician.name
+					}
+				]);
+			});
+
+		LayoutService.reset();
+		LayoutService.setTitle(['Technician', 'Technicians']);
+		LayoutService.getPageHeader().setActionButton('<button type="button" class="btn btn-primary btn-block"><i class="fa fa-check"></i> Save</button>');
+		LayoutService.getPageHeader().setBreadcrumbs([
+			{
+				route: 'administrator-index',
+				displayName: 'Home'
+			},
+			{
+				route: 'administrator-technicians-index',
+				displayName: 'Technicians'
+			},
+			{
+				route: 'administrator-technicians-view',
+				displayName: 'Technician'
+			}
+		]);
+
+		$scope.save = function() {
+			console.log('Update this code to save it.');
+		};
+
+		LayoutService.getPageHeader().onClicked($scope.save);
 	}]);
